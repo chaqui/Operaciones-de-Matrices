ab33aa2e14fb1358a38b2262a80d5dc977a57cee
chaqui <chaqui@chaqui-ubuntu.(none)>
2012-11-05 12:59:10 -0600

    terminanda la interfaz aumentada

--- interfazaumentada.py
+++ interfazaumentada.py
@@ -1,8 +1,3 @@
-'''
-Created on 9/07/2012
-
-@author: josuechaqui
-'''
 #coding: utf-8
 import wx
 class IAumentada(wx.Frame):
@@ -16,7 +11,7 @@ class IAumentada(wx.Frame):
         self.stCredito=wx.StaticText(self,label="------Matriz Aumentada------- 	"+" "+title,pos=(5,self.pos+10),size=(200,self.size))
         self.pos=5
         #---------------Datos De las ecuaciones----------------s
-        self.PCEcuaciones=wx.Panel(self,pos=(0,30),size=(800,200))
+        self.PCEcuaciones=wx.Panel(self,pos=(0,30),size=(800,500))
         #titulo de las ecuaciones
         self.stEcuacion=wx.StaticText(self.PCEcuaciones,label="Ecuaciones:",pos=(5,self.pos),size=(150,self.size))
         self.stNombreCliente=wx.StaticText(self.PCEcuaciones,label="(de la forma ax+by+cz=d)",pos=(131,self.pos),size=(250,self.size))
@@ -33,17 +28,34 @@ class IAumentada(wx.Frame):
         self.stTEcuaciones=wx.StaticText(self.PCEcuaciones,label="Tercera Ecuacion:",pos=(2,self.pos),size=(150,self.size))
         self.txtTEcuaciones=wx.TextCtrl(self.PCEcuaciones,pos=(131,self.pos),size=(350,self.size),name="txtTEcuaciones",id=0)
         self.pos=self.pos+self.size
-        #resultado primera ecuacion
-        self.stREcuacion1=wx.StaticText(self.PCEcuaciones,label="",pos=(131,self.pos),size=(150,self.size))
+        self.stCEcuaciones=wx.StaticText(self.PCEcuaciones,label="Cuarta Ecuacion:",pos=(2,self.pos),size=(150,self.size))
+        self.txtCEcuaciones=wx.TextCtrl(self.PCEcuaciones,pos=(131,self.pos),size=(350,self.size),name="txtTEcuaciones",id=0)
         self.pos=self.pos+self.size
-        #resultado segunda ecuacion
-        self.stREcuacion2=wx.StaticText(self.PCEcuaciones,label="",pos=(131,self.pos),size=(150,self.size))
+        self.stEcuacionq=wx.StaticText(self.PCEcuaciones,label="--------------------------------------------------Respuesta:--------------------------------------------------",pos=(5,self.pos),size=(800,self.size))
         self.pos=self.pos+self.size
-        #resultado tercera ecuacion
-        self.stREcuacion3=wx.StaticText(self.PCEcuaciones,label="",pos=(131,self.pos),size=(150,self.size))
+        self.stX1=wx.StaticText(self.PCEcuaciones,label="a",pos=(131,self.pos),size=(50,self.size))
+        self.stY1=wx.StaticText(self.PCEcuaciones,label="",pos=(185,self.pos),size=(50,self.size))
+        self.stZ1=wx.StaticText(self.PCEcuaciones,label="",pos=(240,self.pos),size=(50,self.size))
+        self.stN1=wx.StaticText(self.PCEcuaciones,label="",pos=(290,self.pos),size=(50,self.size))
+        self.pos=self.pos+self.size
+        self.stX2=wx.StaticText(self.PCEcuaciones,label="",pos=(131,self.pos),size=(50,self.size))
+        self.stY2=wx.StaticText(self.PCEcuaciones,label="",pos=(185,self.pos),size=(50,self.size))
+        self.stZ2=wx.StaticText(self.PCEcuaciones,label="",pos=(240,self.pos),size=(50,self.size))
+        self.stN2=wx.StaticText(self.PCEcuaciones,label="",pos=(290,self.pos),size=(50,self.size))
+        self.pos=self.pos+self.size
+        self.stX3=wx.StaticText(self.PCEcuaciones,label="",pos=(131,self.pos),size=(50,self.size))
+        self.stY3=wx.StaticText(self.PCEcuaciones,label="",pos=(185,self.pos),size=(50,self.size))
+        self.stZ3=wx.StaticText(self.PCEcuaciones,label="",pos=(240,self.pos),size=(50,self.size))
+        self.stN3=wx.StaticText(self.PCEcuaciones,label="",pos=(290,self.pos),size=(50,self.size))
+        self.pos=self.pos+self.size
+        self.stX4=wx.StaticText(self.PCEcuaciones,label="",pos=(131,self.pos),size=(50,self.size))
+        self.stY4=wx.StaticText(self.PCEcuaciones,label="",pos=(185,self.pos),size=(50,self.size))
+        self.stZ4=wx.StaticText(self.PCEcuaciones,label="",pos=(240,self.pos),size=(50,self.size))
+        self.stN4=wx.StaticText(self.PCEcuaciones,label="",pos=(290,self.pos),size=(50,self.size))
         self.txtPEcuacion.Bind(wx.EVT_KEY_UP, self.analizarPEcuacion)
         self.txtSEcuaciones.Bind(wx.EVT_KEY_UP, self.analizarSEcuacion)
         self.txtTEcuaciones.Bind(wx.EVT_KEY_UP, self.analizarTEcuacion)
+        self.txtCEcuaciones.Bind(wx.EVT_KEY_UP, self.analizarCEcuacion)
         self.Centre(True)  # Centrar la ventana en pantalla
         self.Show(True)
     def analizarPEcuacion(self,e):
@@ -56,28 +68,88 @@ class IAumentada(wx.Frame):
                 cadena="".join(cad1)
                 self.txtPEcuacion.SetValue(cadena)
             else:
-                self.stREcuacion1.SetLabel(str(self.analizar(cad1,True)))
+                print cad1
+                self.ec=self.analizar(cad1,True)
+                if len(self.ec[0])==4:
+                    self.stX1.SetLabel(str(self.ec[0][0]))
+                    self.stY1.SetLabel(str(self.ec[0][1]))
+                    self.stZ1.SetLabel(str(self.ec[0][2]))
+                    self.stN1.SetLabel(str(self.ec[0][3]))
+                else:
+                    self.stX1.SetLabel(str(self.ec[0][0]))
+                    self.stY1.SetLabel(str(self.ec[0][1]))
+                    self.stZ1.SetLabel(str(self.ec[0][2]))
+                    self.stN1.SetLabel("")
             if self.txtSEcuaciones.GetValue() != "":
-                self.stREcuacion2.SetLabel(str(self.analizar(list(self.txtSEcuaciones.GetValue()),False)))
+                cad1=self.txtSEcuaciones.GetValue()
+                self.ec=self.analizar(cad1,False)
+                if len(self.ec[0])==4:
+                    self.stX2.SetLabel(str(self.ec[0][0]))
+                    self.stY2.SetLabel(str(self.ec[0][1]))
+                    self.stZ2.SetLabel(str(self.ec[0][2]))
+                    self.stN2.SetLabel(str(self.ec[0][3]))
+                else:
+                    self.stX2.SetLabel(str(self.ec[0][0]))
+                    self.stY2.SetLabel(str(self.ec[0][1]))
+                    self.stZ2.SetLabel(str(self.ec[0][2]))
+                    self.stN2.SetLabel("")
             if self.txtTEcuaciones.GetValue() != "":
-                self.stREcuacion3.SetLabel(str(self.analizar(list(self.txtTEcuaciones.GetValue()),False)))
+                cad1=self.txtTEcuaciones.GetValue()
+                self.ec=self.analizar(cad1,False)
+                if len(self.ec[0])==4:
+                    self.stX3.SetLabel(str(self.ec[0][0]))
+                    self.stY3.SetLabel(str(self.ec[0][1]))
+                    self.stZ3.SetLabel(str(self.ec[0][2]))
+                    self.stN3.SetLabel(str(self.ec[0][3]))
+                else:
+                    self.stX3.SetLabel(str(self.ec[0][0]))
+                    self.stY3.SetLabel(str(self.ec[0][1]))
+                    self.stZ3.SetLabel(str(self.ec[0][2]))
+                    self.stN3.SetLabel("")
+            if self.txtCEcuaciones.GetValue() != "":
+                if self.b:
+                    cad1= self.txtCEcuaciones.GetValue() 
+                    self.ec=self.analizar(cad1,False)
+                    if len(self.ec[0])==4:
+                        self.stX4.SetLabel(str(self.ec[0][0]))
+                        self.stY4.SetLabel(str(self.ec[0][1]))
+                        self.stZ4.SetLabel(str(self.ec[0][2]))
+                        self.stN4.SetLabel(str(self.ec[0][3]))
+                    else:
+                        self.stX4.SetLabel(str(self.ec[0][0]))
+                        self.stY4.SetLabel(str(self.ec[0][1]))
+                        self.stZ4.SetLabel(str(self.ec[0][2]))
+                        self.stN4.SetLabel("")
+                else:
+                    self.stX4.SetLabel("")
+                    self.stY4.SetLabel("")
+                    self.stZ4.SetLabel("")
+                    self.stN4.SetLabel("")
         except Exception, e:
             cad1=[]
-            self.stREcuacion1.SetLabel("")
     def analizarSEcuacion(self,e):
         noAceptadas=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w']
         cad1=list(self.txtSEcuaciones.GetValue())
-        try:
+        try:    
             if cad1[-1] in noAceptadas :
                 print cad1
                 del cad1[-1]
                 cadena="".join(cad1)
                 self.txtSEcuaciones.SetValue(cadena)
             else:
-                self.stREcuacion2.SetLabel(str(self.analizar(cad1,False)))
+                self.ec=self.analizar(cad1,False)
+                if len(self.ec[0])==4:
+                    self.stX2.SetLabel(str(self.ec[0][0]))
+                    self.stY2.SetLabel(str(self.ec[0][1]))
+                    self.stZ2.SetLabel(str(self.ec[0][2]))
+                    self.stN2.SetLabel(str(self.ec[0][3]))
+                else:
+                    self.stX2.SetLabel(str(self.ec[0][0]))
+                    self.stY2.SetLabel(str(self.ec[0][1]))
+                    self.stZ2.SetLabel(str(self.ec[0][2]))
+                    self.stN2.SetLabel("")
         except Exception, e:
             cad1=[]
-            self.stREcuacion2.SetLabel("")
     def analizarTEcuacion(self,e):
         noAceptadas=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w']
         cad1=list(self.txtTEcuaciones.GetValue())
@@ -88,10 +160,43 @@ class IAumentada(wx.Frame):
                 cadena="".join(cad1)
                 self.txtTEcuaciones.SetValue(cadena)
             else:
-                self.stREcuacion3.SetLabel(str(self.analizar(cad1,False)))
+                self.ec=self.analizar(cad1,False)
+                if len(self.ec[0])==4:
+                    self.stX3.SetLabel(str(self.ec[0][0]))
+                    self.stY3.SetLabel(str(self.ec[0][1]))
+                    self.stZ3.SetLabel(str(self.ec[0][2]))
+                    self.stN3.SetLabel(str(self.ec[0][3]))
+                else:
+                    self.stX3.SetLabel(str(self.ec[0][0]))
+                    self.stY3.SetLabel(str(self.ec[0][1]))
+                    self.stZ3.SetLabel(str(self.ec[0][2]))
+                    self.stN3.SetLabel("")
         except Exception, e:
             cad1=[]
-            self.stREcuacion3.SetLabel("")
+    def analizarCEcuacion(self,e):
+        if self.b:
+            noAceptadas=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w']
+            cad1=list(self.txtCEcuaciones.GetValue())
+            try:
+                if cad1[-1] in noAceptadas :
+                    print cad1
+                    del cad1[-1]
+                    cadena="".join(cad1)
+                    self.txtCEcuaciones.SetValue(cadena)
+                else:
+                    self.ec=self.analizar(cad1,False)
+                    if len(self.ec[0])==4:
+                        self.stX4.SetLabel(str(self.ec[0][0]))
+                        self.stY4.SetLabel(str(self.ec[0][1]))
+                        self.stZ4.SetLabel(str(self.ec[0][2]))
+                        self.stN4.SetLabel(str(self.ec[0][3]))
+                    else:
+                        self.stX4.SetLabel(str(self.ec[0][0]))
+                        self.stY4.SetLabel(str(self.ec[0][1]))
+                        self.stZ4.SetLabel(str(self.ec[0][2]))
+                        self.stN4.SetLabel("")
+            except Exception, e:
+                cad1=[]
     def analizar(self,lista,k):
         print lista
         x=0
@@ -167,8 +272,7 @@ class IAumentada(wx.Frame):
         self.fila.append(n)
         matrizr.append(self.fila)
         print "la Matriz Aumentada es la siguiente:"
-        for x in matrizr:
-            print x
+        print matrizr
         return matrizr
 app = wx.App(False)
 APP_TITLE=" --Python--"
